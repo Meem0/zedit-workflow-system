48be67ba2330805b2672169baaa898b218edb81e
Meem0 <themeem0@gmail.com>
2020-03-21 18:57:07 -0700

    Break workflow processing functions out into service

--- src/runWorkflow.js
+++ src/runWorkflow.js
@@ -11,132 +11,6 @@ ngapp.directive('runWorkflow', function () {
 });
 
 ngapp.controller('runWorkflowController', function ($scope, workflowService) {
-    class StageRoadmapEntry {
-        constructor(stage, workflow) {
-            this._stageStates = {
-                unknown: -1,
-                complete: 0,
-                incomplete: 1,
-                unavailable: 2
-            };
-
-            this.stage = stage;
-            this.workflow = workflow;
-            this._state = this._stageStates.unknown;
-        }
-
-        isComplete() { return this._state === this._stageStates.complete; }
-        isIncomplete() { return this._state === this._stageStates.incomplete; }
-        isAvailable() { return this.isComplete() || this.isIncomplete(); }
-        setComplete() { this._state = this._stageStates.complete; }
-        setIncomplete() { this._state = this._stageStates.incomplete; }
-        setUnavailable() { this._state = this._stageStates.unavailable; }
-    };
-
-    let getViewFromStage = function(stage) {
-        if (stage && stage.view) {
-            return {
-                name: stage.view,
-                ...workflowService.getView(stage.view)
-            };
-        }
-    };
-
-    let isRoadmapComplete = function(stageRoadmap) {
-        return stageRoadmap.every(stage => stage.isComplete());
-    };
-    
-    let getNextStage = function(workflow, currentStageName, workflowModel, foundCurrentStage = false) {
-        let nextStage;
-        let nextStageWorkflow;
-        for (const stage of workflow.stages) {
-            if (!stage || (stage.shouldInclude && !stage.shouldInclude(workflowModel))) {
-                continue;
-            }
-            if (stage.subflow) {
-                // recurse into subflow
-                const subflow = workflowService.getWorkflow(stage.subflow);
-                if (subflow) {
-                    ({foundCurrentStage, nextStage, nextStageWorkflow} = getNextStage(subflow, currentStageName, workflowModel, foundCurrentStage));
-                }
-            }
-            else if (!currentStageName || foundCurrentStage) {
-                nextStage = stage;
-                nextStageWorkflow = workflow;
-            }
-            else if (!foundCurrentStage) {
-                foundCurrentStage = currentStageName === stage.name;
-            }
-            if (nextStage) {
-                break;
-            }
-        }
-        return {foundCurrentStage, nextStage, nextStageWorkflow};
-    };
-
-    let getInputForStage = function(stage, workflowModel) {
-        const stageInput = stage && stage.input ? stage.input : {};
-
-        const stageView = getViewFromStage(stage);
-        const inputKeys = stageView && stageView.requireInput ? stageView.requireInput : [];
-        const modelInput = inputKeys.reduce((input, key) => {
-            input[key] = workflowModel[key];
-            return input;
-        }, {});
-
-        return Object.assign({}, stageInput, modelInput);
-    };
-
-    let processStages = function(workflow, workflowInput, stageModels) {
-        let stageName = '';
-        let workflowModel = {...workflowInput};
-        let stageInputs = {};
-        let stageRoadmap = [];
-        while (true) {
-            const {nextStage: stage, nextStageWorkflow: stageWorkflow} = getNextStage(workflow, stageName, workflowModel);
-            if (!stage) {
-                // all stages of workflow are complete
-                break;
-            }
-
-            stageName = stage.name;
-            const stageView = getViewFromStage(stage);
-            if (!stageView || !stageView.process) {
-                console.error(`Could not get valid view from stage ${stageName}`);
-                continue;
-            }
-
-            let stageRoadmapEntry = new StageRoadmapEntry(stage, stageWorkflow);
-            stageRoadmap.push(stageRoadmapEntry);
-
-            const stageInput = getInputForStage(stage, workflowModel);
-
-            stageInputs[stageName] = angular.copy(stageInput);
-
-            let stageModel = stageModels[stageName];
-            if (!stageModel) {
-                stageModel = {};
-                stageModels[stageName] = stageModel;
-            }
-            
-            const stageOutput = stageView.process(stageInput, stageModel);
-            if (!stageOutput) {
-                // a stage of the workflow is incomplete; stop processing the workflow
-                stageRoadmapEntry.setIncomplete();
-                break;
-            }
-            
-            stageRoadmapEntry.setComplete();
-            Object.assign(workflowModel, stageOutput);
-        }
-
-        return {
-            stageRoadmap,
-            stageInputs,
-            workflowModel
-        };
-    };
-
     let addUnavailableRoadmapEntries = function(newRoadmap, oldRoadmap) {
         if (newRoadmap.length >= oldRoadmap.length) {
             return newRoadmap;
@@ -152,8 +26,12 @@ ngapp.controller('runWorkflowController', function ($scope, workflowService) {
         return newRoadmap.concat(additionalRoadmapEntries);
     };
 
+    let isRoadmapComplete = function(stageRoadmap) {
+        return stageRoadmap.every(stage => stage.isComplete());
+    };
+
     let processWorkflow = function(workflow, workflowInput, stageModels, previousStageRoadmap) {
-        let {stageRoadmap, stageInputs, workflowModel} = processStages(workflow, workflowInput, stageModels);
+        let {stageRoadmap, stageInputs, workflowModel} = workflowService.processWorkflow(workflow, workflowInput, stageModels);
         
         // e.g. workflow has 5 stages, stage 3 is incomplete
         //  if the previousStageRoadmap had stages after 3, we want those appended to the roadmap
@@ -189,7 +67,7 @@ ngapp.controller('runWorkflowController', function ($scope, workflowService) {
         $scope.stageRoadmap.forEach(stageRoadmapEntry => {
             const stageName = stageRoadmapEntry.stage.name;
             if (!$scope.loadedViews[stageName]) {
-                const stageView = getViewFromStage(stageRoadmapEntry.stage);
+                const stageView = workflowService.getView(stageRoadmapEntry.stage.view);
                 if (stageView) {
                     $scope.loadedViews[stageName] = {
                         stageName,
--- src/workflowService.js
+++ src/workflowService.js
@@ -3,6 +3,128 @@ ngapp.service('workflowService', function() {
         workflows = {},
         views = {};
 
+    class StageRoadmapEntry {
+        constructor(stage, workflow) {
+            this._stageStates = {
+                unknown: -1,
+                complete: 0,
+                incomplete: 1,
+                unavailable: 2
+            };
+
+            this.stage = stage;
+            this.workflow = workflow;
+            this._state = this._stageStates.unknown;
+        }
+
+        isComplete() { return this._state === this._stageStates.complete; }
+        isIncomplete() { return this._state === this._stageStates.incomplete; }
+        isAvailable() { return this.isComplete() || this.isIncomplete(); }
+        setComplete() { this._state = this._stageStates.complete; }
+        setIncomplete() { this._state = this._stageStates.incomplete; }
+        setUnavailable() { this._state = this._stageStates.unavailable; }
+    };
+
+    let getViewFromStage = function(stage) {
+        if (stage && stage.view) {
+            return {
+                name: stage.view,
+                ...views[stage.view]
+            };
+        }
+    };
+    
+    let getNextStage = function(workflow, currentStageName, workflowModel, foundCurrentStage = false) {
+        let nextStage;
+        let nextStageWorkflow;
+        for (const stage of workflow.stages) {
+            if (!stage || (stage.shouldInclude && !stage.shouldInclude(workflowModel))) {
+                continue;
+            }
+            if (stage.subflow) {
+                // recurse into subflow
+                const subflow = workflows[stage.subflow];
+                if (subflow) {
+                    ({foundCurrentStage, nextStage, nextStageWorkflow} = getNextStage(subflow, currentStageName, workflowModel, foundCurrentStage));
+                }
+            }
+            else if (!currentStageName || foundCurrentStage) {
+                nextStage = stage;
+                nextStageWorkflow = workflow;
+            }
+            else if (!foundCurrentStage) {
+                foundCurrentStage = currentStageName === stage.name;
+            }
+            if (nextStage) {
+                break;
+            }
+        }
+        return {foundCurrentStage, nextStage, nextStageWorkflow};
+    };
+
+    let getInputForStage = function(stage, workflowModel) {
+        const stageInput = stage && stage.input ? stage.input : {};
+
+        const stageView = getViewFromStage(stage);
+        const inputKeys = stageView && stageView.requireInput ? stageView.requireInput : [];
+        const modelInput = inputKeys.reduce((input, key) => {
+            input[key] = workflowModel[key];
+            return input;
+        }, {});
+
+        return Object.assign({}, stageInput, modelInput);
+    };
+
+    this.processWorkflow = function(workflow, workflowInput, stageModels) {
+        let stageName = '';
+        let workflowModel = {...workflowInput};
+        let stageInputs = {};
+        let stageRoadmap = [];
+        while (true) {
+            const {nextStage: stage, nextStageWorkflow: stageWorkflow} = getNextStage(workflow, stageName, workflowModel);
+            if (!stage) {
+                // all stages of workflow are complete
+                break;
+            }
+
+            stageName = stage.name;
+            const stageView = getViewFromStage(stage);
+            if (!stageView || !stageView.process) {
+                console.error(`Could not get valid view from stage ${stageName}`);
+                continue;
+            }
+
+            let stageRoadmapEntry = new StageRoadmapEntry(stage, stageWorkflow);
+            stageRoadmap.push(stageRoadmapEntry);
+
+            const stageInput = getInputForStage(stage, workflowModel);
+
+            stageInputs[stageName] = angular.copy(stageInput);
+
+            let stageModel = stageModels[stageName];
+            if (!stageModel) {
+                stageModel = {};
+                stageModels[stageName] = stageModel;
+            }
+            
+            const stageOutput = stageView.process(stageInput, stageModel);
+            if (!stageOutput) {
+                // a stage of the workflow is incomplete; stop processing the workflow
+                stageRoadmapEntry.setIncomplete();
+                break;
+            }
+            
+            stageRoadmapEntry.setComplete();
+            Object.assign(workflowModel, stageOutput);
+        }
+
+        return {
+            stageRoadmap,
+            stageInputs,
+            workflowModel
+        };
+    };
+
     this.addModule = function({name, ...module}) {
         modules[name] = {name: name, ...module};
     };
